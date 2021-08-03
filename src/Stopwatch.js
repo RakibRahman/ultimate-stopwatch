8616b303aa29a31c27719d621462b587a3bee258
RakibRahman <md.rr.talukder@gmail.com>
2021-08-03 12:34:21 +0600

    sound effect added

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import "./Stopwatch.css";
+import Tick from "./tick.mp3";
 const Stopwatch = (_) => {
   const [timer, setTimer] = useState(0);
-  const [counter, setCounter] = useState(false);
-  const currentTime = useRef(0);
+  const [counter, setCounter] = useState(true);
+  const TickTock = new Audio(Tick);
   useEffect(() => (document.title = "Ultimate Stopwatch"));
   useEffect(() => {
     let interval = null;
-    if (counter) {
+    if (!counter) {
       interval = setInterval(() => {
-        currentTime.current++;
-        setTimer(currentTime.current++);
+        setTimer((t) => t + 1);
       }, 1000);
+      TickTock.play();
+      TickTock.loop = true;
     }
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+      TickTock.pause();
+    };
   }, [counter]);
-  const resetStopwatch = () => {
+
+  const formattedTime = new Date(timer * 1000).toISOString().substr(11, 8);
+  const toggleStopwatch = (_) => {
+    setCounter((e) => !e);
+    const btn = document.querySelector("#toggleBtn");
+    if (btn.innerText === "Start") {
+      btn.innerText = "Pause";
+      btn.style.backgroundColor = "#d35400";
+    } else {
+      btn.innerText = "Start";
+      btn.style.backgroundColor = "#2ecc71";
+    }
+  };
+  const resetStopwatch = (_) => {
     setTimer(0);
   };
-  const startStopwatch = (_) => {
-    setCounter(!counter);
-  };
   return (
-    <div>
-      <h1>Ultimate Stopwatch</h1>
-      <p>{timer}</p>
-      <button onClick={startStopwatch}>Start/Pause</button>
-      <button onClick={resetStopwatch}>Reset</button>
+    <div className="wrapper">
+      <h1 className="title">Ultimate Stopwatch</h1>
+      <p className="watch">{formattedTime}</p>
+      <button id="toggleBtn" onClick={toggleStopwatch}>
+        Start
+      </button>
+      <button id="resetBtn" onClick={resetStopwatch}>
+        Reset
+      </button>
     </div>
   );
 };
