12a2a6595771ffd8b2a0c3feccb504266cc9ed3d
RakibRahman <md.rr.talukder@gmail.com>
2021-08-03 12:18:10 +0600

    styles applied

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 const Stopwatch = (_) => {
   const [timer, setTimer] = useState(0);
+  const [counter, setCounter] = useState(false);
   const currentTime = useRef(0);
+  useEffect(() => (document.title = "Ultimate Stopwatch"));
   useEffect(() => {
-    const interval = setInterval(() => {
-      currentTime.current++;
-      setTimer(currentTime.current++);
-      console.log("set timer");
-    }, 1000);
+    let interval = null;
+    if (counter) {
+      interval = setInterval(() => {
+        currentTime.current++;
+        setTimer(currentTime.current++);
+      }, 1000);
+    }
     return () => clearInterval(interval);
-  }, []);
+  }, [counter]);
   const resetStopwatch = () => {
-    console.log("ok");
+    setTimer(0);
+  };
+  const startStopwatch = (_) => {
+    setCounter(!counter);
   };
   return (
     <div>
       <h1>Ultimate Stopwatch</h1>
       <p>{timer}</p>
-      <button>Start</button>
+      <button onClick={startStopwatch}>Start/Pause</button>
       <button onClick={resetStopwatch}>Reset</button>
     </div>
   );
